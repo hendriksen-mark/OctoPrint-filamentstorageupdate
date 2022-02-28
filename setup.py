3e303153d7a19d02b24f407410a7ab475ec61863
Mark <hendriksen-mark@hotmail.com>
2022-02-28 15:12:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 plugin_identifier = "filamentstorageupdate"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "OctoPrint_filamentstorageupdate"
+plugin_package = "octoprint_filamentstorageupdate"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
